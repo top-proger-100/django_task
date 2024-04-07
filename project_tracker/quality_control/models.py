155b1a7962ae4fe521c789b5000d963a40899958
Ostapenko Igor <skeleton1212@yandex.ru>
2024-04-07 12:08:58 +0300

    added model BugReport

--- project_tracker/quality_control/models.py
+++ project_tracker/quality_control/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+from tasks.models import Project, Task
+
+
 # Create your models here.
+
+
+class BugReport(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.CASCADE,
+    )
+    task = models.ForeignKey(
+        Task,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    status = [
+        ('New', 'Новая'),
+        ('In_progress', 'В работе'),
+        ('Completed', 'Завершена'),
+    ]
+    priority = models.IntegerField(choices=[
+        (1, '1 балл'),
+        (2, '2 балла'),
+        (3, '3 балла'),
+        (4, '4 балла'),
+        (5, '5 баллов')
+    ])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
