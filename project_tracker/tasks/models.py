efc4388d88b6a3144b65f59b527005415289d4e1
Ostapenko Igor <skeleton1212@yandex.ru>
2024-04-11 19:47:05 +0300

    added url tasks

--- project_tracker/tasks/models.py
+++ project_tracker/tasks/models.py
@@ -8,6 +8,8 @@ class Project(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    ar = []
+    ar.insert(1, 12)
 
     def __str__(self):
         return self.name
