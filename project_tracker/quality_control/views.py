9786101c917223b90992c4ccd96fd2135f67a7ac
Ostapenko Igor <skeleton1212@yandex.ru>
2024-04-12 19:25:41 +0300

    fixed views

--- project_tracker/quality_control/views.py
+++ project_tracker/quality_control/views.py
@@ -11,19 +11,12 @@ from quality_control.models import BugReport, FeatureRequest
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        bug_list_url = reverse('quality_control:bugs')
-        feature_list_url = reverse('quality_control:features')
-        html = f"<h1>Система контроля качества</h1><a href='{bug_list_url}'>Список всех багов</a><br><a href='{feature_list_url}'>Запросы на улучшение</a>"
-        return HttpResponse(html)
+        return render(request, 'quality_control/index.html')
 
 
 def bug_list(request):
     bug_report = BugReport.objects.all()
-    html = f"<h1>Cписок отчетов об ошибках</h1><ul>"
-    for record in bug_report:
-        html += f'<li><a href="{record.id}">{record.title}</a> | {record.status}</li>'
-    html += '</ul>'
-    return HttpResponse(html)
+    return render(request, 'quality_control/bugs.html', {'bug_list': bug_report})
 
 
 class BugDetailView(DetailView):
@@ -46,14 +39,5 @@ class FeatureDetailView(DetailView):
 
     def get(self, request, *args, **kwargs):
         obj = self.get_object()
-        html = f'''
-            <h1>Детали улучшения {obj.id}</h1>
-            <p>Название: {obj.title}</p>
-            <p>Описание: {obj.description}</p>
-            <p>Статус: {obj.status}</p>
-            <p>Приоритет: {obj.priority}</p>
-            <p>Связанный проект: {obj.project}</p>
-            <p>Связанная задача: {obj.task}</p>
-        '''
-        return HttpResponse(html)
+        return render(request, 'quality_control/feature_detail.html', {'feature': obj})
 
