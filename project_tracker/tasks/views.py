e86055828bd63d3489cb6039260658711a5eb1b3
Ostapenko Igor <skeleton1212@yandex.ru>
2024-04-12 16:34:18 +0300

    added FBV tasks

--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -10,10 +10,11 @@ from .models import Project, Task
 # Create your views here.
 
 
-# def index(request):
-#     projects_list_url = reverse('tasks:projects_list')
-#     html = f"<h1>Страница приложения tasks</h1><a href='{projects_list_url}'>Список проектов</a>"
-#     return HttpResponse(html)
+def index(request):
+    # projects_list_url = reverse('tasks:projects_list')
+    # html = f"<h1>Страница приложения tasks</h1><a href='{projects_list_url}'>Список проектов</a>"
+    # return HttpResponse(html)
+    return render(request, 'tasks/index.html')
 
 
 class IndexView(View):
@@ -23,13 +24,9 @@ class IndexView(View):
         return HttpResponse(html)
 
 
-# def projects_list(request):
-#     projects = Project.objects.all()
-#     projects_html = '<h1>Список проектов</h1><ul>'
-#     for project in projects:
-#         projects_html += f'<li><a href="{project.id}/">{project.name}</a></li>'
-#     projects_html += '</ul>'
-#     return HttpResponse(projects_html)
+def projects_list(request):
+    projects = Project.objects.all()
+    return render(request, 'tasks/projects_list.html', {'project_list': projects})
 
 
 class ProjectsListView(ListView):
@@ -48,15 +45,9 @@ def another_page(request):
     return HttpResponse("Это другая страница приложения tasks.")
 
 
-# def project_detail(request, project_id):
-#     project = get_object_or_404(Project, id=project_id)
-#     tasks = project.tasks.all()
-#     response_html = f'<h1>{project.name}</h1><p>{project.description}</p>'
-#     response_html += '<h2>Задачи</h2><ul>'
-#     for task in tasks:
-#         response_html += f'<li><a href="tasks/{task.id}/">{task.name}</a></li>'
-#     response_html += '</ul>'
-#     return HttpResponse(response_html)
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    return render(request, 'tasks/project_detail.html', {'project': project})
 
 
 class ProjectDetailView(DetailView):
@@ -75,11 +66,9 @@ class ProjectDetailView(DetailView):
         return HttpResponse(response_html)
 
 
-# def task_detail(request, project_id, task_id):
-#     project = get_object_or_404(Project, id=project_id)
-#     task = get_object_or_404(Task, id=task_id, project=project)
-#     response_html = f'<h1>{task.name}</h1><p>{task.description}</p>'
-#     return HttpResponse(response_html)
+def task_detail(request, project_id, task_id):
+    task = get_object_or_404(Task, id=task_id, project_id=project_id)
+    return render(request, 'tasks/task_detail.html', {'task': task})
 
 
 class TaskDetailView(DetailView):
