4b63422b6ddde1d7bbb95898b43ba8dff4e53c19
Ostapenko Igor <skeleton1212@yandex.ru>
2024-04-12 14:58:39 +0300

    added list of projects

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+tfrom django.urls import path
 from tasks import views
 
 app_name = 'tasks'
 
 urlpatterns = [
     path('', views.index),
-    path('another/', views.another_page, name='another_page'),
+    path('projects/', views.projects_list, name='projects_list'),
+    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+    path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
 ]
--- project_tracker/tasks/views.py
+++ project_tracker/tasks/views.py
@@ -1,17 +1,44 @@
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from django.shortcuts import render
 from django.urls import reverse
+from .models import Project, Task
+
 
 # Create your views here.
 
 
 def index(request):
-    another_page_url = reverse('tasks:another_page')
-    quality_control_page_url = reverse('quality_control:start')
-    html = (f"<h1>Страница приложения tasks</h1><a href='{another_page_url}'>Перейти на другую страницу</a><br>"
-            f"<a href='{quality_control_page_url}'>Перейти на страницу приложения quality_control</a>")
+    projects_list_url = reverse('tasks:projects_list')
+    html = f"<h1>Страница приложения tasks</h1><a href='{projects_list_url}'>Список проектов</a>"
     return HttpResponse(html)
+
+
+def projects_list(request):
+    projects = Project.objects.all()
+    projects_html = '<h1>Список проектов</h1><ul>'
+    for project in projects:
+        projects_html += f'<li><a href="{project.id}/">{project.name}</a></li>'
+    projects_html += '</ul>'
+    return HttpResponse(projects_html)
 
 
 def another_page(request):
     return HttpResponse("Это другая страница приложения tasks.")
+
+
+def project_detail(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    tasks = project.tasks.all()
+    response_html = f'<h1>{project.name}</h1><p>{project.description}</p>'
+    response_html += '<h2>Задачи</h2><ul>'
+    for task in tasks:
+        response_html += f'<li><a href="tasks/{task.id}/">{task.name}</a></li>'
+    response_html += '</ul>'
+    return HttpResponse(response_html)
+
+
+def task_detail(request, project_id, task_id):
+    project = get_object_or_404(Project, id=project_id)
+    task = get_object_or_404(Task, id=task_id, project=project)
+    response_html = f'<h1>{task.name}</h1><p>{task.description}</p>'
+    return HttpResponse(response_html)
