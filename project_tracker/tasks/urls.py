66afe68914b6b22e940512559444aa263549f3e1
Ostapenko Igor <skeleton1212@yandex.ru>
2024-04-12 15:09:44 +0300

    task class-based views

--- project_tracker/tasks/urls.py
+++ project_tracker/tasks/urls.py
@@ -1,11 +1,18 @@
-tfrom django.urls import path
+from django.urls import path
 from tasks import views
 
 app_name = 'tasks'
 
+# urlpatterns = [
+#     path('', views.index),
+#     path('projects/', views.projects_list, name='projects_list'),
+#     path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
+#     path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+# ]
+
 urlpatterns = [
-    path('', views.index),
-    path('projects/', views.projects_list, name='projects_list'),
-    path('projects/<int:project_id>/', views.project_detail, name='project_detail'),
-    path('projects/<int:project_id>/tasks/<int:task_id>/', views.task_detail, name='task_detail'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('projects/', views.ProjectsListView.as_view(), name='projects_list'),
+    path('projects/<int:project_id>/', views.ProjectDetailView.as_view(), name='project_detail'),
+    path('projects/<int:project_id>/tasks/<int:task_id>/', views.TaskDetailView.as_view(), name='task_detail'),
 ]
